df4f346ed364cebb8e2d05e25eec8bfa96e4297c
Djavan Cackley <djavancackley@Djavans-MBP.home>
2018-12-13 20:46:12 -1000

    Calculator Complete

--- calculator.js
+++ calculator.js
@@ -6,6 +6,10 @@
  * @return {object} `calculator` object that can be used
  */
 
+ var calculatorModule = function(){   
+    var memory = 0;
+    var total = 0;
+
 
   /**
    * sets the `total` to the number passed in
@@ -13,11 +17,24 @@
    * @return { Number }    current total
    */
 
+var load = function(num){
+    if(typeof num === "number"){
+    total = num;
+    return total;
+    }else{num !== "number"
+        throw error
+    }
+}
+    
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
+
+var getTotal = function(){
+    return total
+}
 
 
   /**
@@ -25,41 +42,100 @@
    * @param { Number } x
    */
 
+   var add = function(num){
+    if(typeof num === "number"){
+        total += num;
+        return total;
+        }else{num !== "number"
+            throw error
+        }
+   }
 
+   
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
 
+   var subtract = function(num){
+   if(typeof num === "number"){
+    total -= num;
+    return total;
+    }else{num !== "number"
+        throw error
+    }
+}
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
 
+   var multiply = function(num){
+    if(typeof num === "number"){
+        total *= num;
+        return total;
+        }else{num !== "number"
+            throw error
+        }
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   var divide = function(num){
+    if(typeof num === "number"){
+        total /= num;
+        return total;
+        }else{num !== "number"
+            throw error
+        }
+   }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+   var recallMemory = function(){
+       return memory
+   }
+
 
   /**
    * Stores the value of `total` to `memory`
    */
+
+   var saveMemory = function(){
+        memory = total
+        return memory
+   }
 
 
   /**
    * Clear the value stored at `memory`
    */
 
+   var clearMemory = function(){
+       memory = 0
+       return memory
+   }
+
   /**
    * Validation
    */
 
+  return {
+      load,
+      getTotal,
+      add,
+      subtract,
+      multiply,
+      divide,
+      recallMemory,
+      saveMemory,
+      clearMemory
+    }
+};  
